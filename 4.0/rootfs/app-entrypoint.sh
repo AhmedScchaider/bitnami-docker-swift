dc7672486648b2bea06a79d730d8bd5456e4abc1
David Barranco <dbarranco@bitnami.com>
2017-12-13 10:44:07 +0100

    Update swift branches to maintain 3.1 and 4.0

---
+++ 4.0/rootfs/app-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+. /opt/bitnami/base/functions
+
+print_welcome_page
+
+#set app name to ENV or default
+SWIFT_APP="my-app"
+COMMAND="$@"
+
+if [ "$APP_NAME" != "" ]; then
+  SWIFT_APP=$APP_NAME
+fi
+
+PROJECT_DIRECTORY=/app/$SWIFT_APP
+
+app_present() {
+  [ -d $PROJECT_DIRECTORY ]
+}
+
+if [ "$1" == "swift" -a "$2" == "app" -a "$3" == "start" ]; then
+  if ! app_present; then
+    log "Creating example Swift application"
+    mkdir $PROJECT_DIRECTORY
+    cd $PROJECT_DIRECTORY
+    swift-package init --type executable
+    swift build
+    log "Swift app created"
+  else
+    log "App already created"
+    cd $PROJECT_DIRECTORY
+    swift build
+  fi
+  COMMAND=$PROJECT_DIRECTORY/.build/debug/$SWIFT_APP
+fi
+
+exec tini -- $COMMAND
